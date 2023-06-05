b4b1a236a1b10a824e5c778e638d0cadd3bc0798
CyberRock66 <vasylhryniuk6@gmail.com>
2023-06-05 12:58:34 +0100

    [added] orders template

--- src/components/Basket/Basket.tsx
+++ src/components/Basket/Basket.tsx
@@ -2,17 +2,23 @@
 
 import { useCartStore } from '@/store/useCart';
 import { OrdersTemplate } from '@/templates/OrdersTemplate/OrdersTemplate';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BsBasketFill } from 'react-icons/bs';
 
 export const Basket = () => {
   const cartTotal = useCartStore((state) => state.totalItems);
+  const [total, setTotal] = useState(0);
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    setTotal(cartTotal);
+  }, [cartTotal]);
 
   return (
     <button type="button" onClick={() => setOpen(true)}>
       <BsBasketFill />
-      <div>{cartTotal}</div>
+      {total === 0 ? null : <div>{cartTotal}</div>}
+
       <OrdersTemplate open={open} setOpen={setOpen} />
     </button>
   );
