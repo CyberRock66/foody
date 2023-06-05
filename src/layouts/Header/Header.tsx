3f71203afde75d52a44d4946cebd8d427b396de4
CyberRock66 <vasylhryniuk6@gmail.com>
2023-06-05 21:28:28 +0100

    [added] github link

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { Basket } from '@/components';
+import { BsGithub } from 'react-icons/bs';
 import styles from './Header.module.scss';
 
 export const Header = () => (
@@ -6,6 +7,18 @@ export const Header = () => (
     <div className="container_preview">
       <div className={styles.header_wrapper}>
         <Basket />
+
+        <div>
+          <a
+            href="https://github.com/CyberRock66/foody"
+            target="_blank"
+            className="flex items-center ml-64"
+            rel="noopener noreferrer"
+          >
+            <span className="mr-2 font-semibold underline">Github</span>
+            <BsGithub />
+          </a>
+        </div>
       </div>
     </div>
   </header>
